399b5bafdaa58f815f96c109a90d8dd8d07d9613
Jason Reilly <jdr0887@gmail.com>
2017-03-29 09:39:47 -0400

    adding test for known hgvs expressions

--- gerese4j-build-37-2/src/test/java/org/renci/gerese4j/Scratch.java
+++ gerese4j-build-37-2/src/test/java/org/renci/gerese4j/Scratch.java
@@ -1,10 +1,13 @@
 package org.renci.gerese4j;
 
+import static org.junit.Assert.assertTrue;
+
 import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.util.Map;
 import java.util.zip.GZIPInputStream;
 
+import org.apache.commons.lang3.Range;
 import org.junit.Test;
 import org.renci.gerese4j.build_37_2.GeReSe4jManager;
 import org.renci.gerese4j.core.ReferenceSequence;
@@ -13,7 +16,6 @@ public class Scratch {
 
     @Test
     public void scratch() {
-
         Map<String, ReferenceSequence> referenceSequenceMap = null;
         long start = System.currentTimeMillis();
         try (InputStream is = GeReSe4jManager.class.getClassLoader().getResourceAsStream("org/renci/gerese4j/reference_sequences_38.7.ser");
@@ -26,6 +28,25 @@ public class Scratch {
         long end = System.currentTimeMillis();
         System.out.println(String.format("duration: %d seconds", (end - start) / 1000));
         System.out.println(referenceSequenceMap.keySet().size());
+
+    }
+
+    @Test
+    public void testPosition() throws Exception {
+        GeReSe4jManager gereseMgr = GeReSe4jManager.getInstance();
+
+        // grabbed these hgvs expressions from clinvar
+        // NC_000004.11:g.4861958C&gt;A
+        assertTrue("C".equals(gereseMgr.getBase("NC_000004.11", 4861958, false)));
+
+        // NC_000008.10:g.67091182G&gt;T
+        assertTrue("G".equals(gereseMgr.getBase("NC_000008.10", 67091182, false)));
+
+        // NC_000019.9:g.41859120delG
+        assertTrue("G".equals(gereseMgr.getBase("NC_000019.9", 41859120, false)));
+
+        // NC_000019.9:g.41858912_41858920dupAGCAGCAGC
+        assertTrue("AGCAGCAGC".equals(gereseMgr.getRegion("NC_000019.9", Range.between(41858912, 41858920), true)));
 
     }
 
--- gerese4j-build-38-7/src/test/java/org/renci/gerese4j/Scratch.java
+++ gerese4j-build-38-7/src/test/java/org/renci/gerese4j/Scratch.java
@@ -1,10 +1,13 @@
 package org.renci.gerese4j;
 
+import static org.junit.Assert.assertTrue;
+
 import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.util.Map;
 import java.util.zip.GZIPInputStream;
 
+import org.apache.commons.lang3.Range;
 import org.junit.Test;
 import org.renci.gerese4j.build_38_7.GeReSe4jManager;
 import org.renci.gerese4j.core.ReferenceSequence;
@@ -13,7 +16,6 @@ public class Scratch {
 
     @Test
     public void scratch() {
-
         Map<String, ReferenceSequence> referenceSequenceMap = null;
         long start = System.currentTimeMillis();
         try (InputStream is = GeReSe4jManager.class.getClassLoader().getResourceAsStream("org/renci/gerese4j/reference_sequences_38.7.ser");
@@ -26,6 +28,25 @@ public class Scratch {
         long end = System.currentTimeMillis();
         System.out.println(String.format("duration: %d seconds", (end - start) / 1000));
         System.out.println(referenceSequenceMap.keySet().size());
+
+    }
+
+    @Test
+    public void testPosition() throws Exception {
+        GeReSe4jManager gereseMgr = GeReSe4jManager.getInstance();
+
+        // grabbed these hgvs expressions from clinvar
+        // NC_000004.12:g.4860231C>A
+        assertTrue("C".equals(gereseMgr.getBase("NC_000004.12", 4860231, false)));
+
+        // NC_000008.11:g.66178947G&gt;T
+        assertTrue("G".equals(gereseMgr.getBase("NC_000008.11", 66178947, false)));
+
+        // NC_000019.10:g.41353215delG
+        assertTrue("G".equals(gereseMgr.getBase("NC_000019.10", 41353215, false)));
+
+        // NC_000019.10:g.41353007_41353015dupAGCAGCAGC
+        assertTrue("AGCAGCAGC".equals(gereseMgr.getRegion("NC_000019.10", Range.between(41353007, 41353015), true)));
 
     }
 
