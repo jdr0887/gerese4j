be9275dbdc350c7f7de9b2c763efeae029fd375b
Jason Reilly <jdr0887@gmail.com>
2017-04-04 08:30:17 -0400

    initial commit

---
+++ gerese4j-build-36-3/src/test/java/org/renci/gerese4j/Scratch.java
@@ -0,0 +1,36 @@
+package org.renci.gerese4j;
+
+import static org.junit.Assert.assertTrue;
+
+import org.apache.commons.lang3.Range;
+import org.junit.Test;
+import org.renci.gerese4j.build_36_3.GeReSe4jBuild_36_3_Manager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Scratch {
+
+    private static final Logger logger = LoggerFactory.getLogger(Scratch.class);
+
+    @Test
+    public void testPosition() throws Exception {
+        logger.info("start initializing");
+        GeReSe4jBuild_36_3_Manager gereseMgr = GeReSe4jBuild_36_3_Manager.getInstance();
+        logger.info("finish initializing");
+
+        // grabbed these hgvs expressions from clinvar
+        assertTrue("C".equals(gereseMgr.getBase("NC_000004.12", 4860231, false)));
+        logger.info("finish search for NC_000004.12:g.4860231C>A");
+
+        assertTrue("G".equals(gereseMgr.getBase("NC_000008.11", 66178947, false)));
+        logger.info("finish search for NC_000008.11:g.66178947G>T");
+
+        assertTrue("G".equals(gereseMgr.getBase("NC_000019.10", 41353215, false)));
+        logger.info("finish search for NC_000019.10:g.41353215delG");
+
+        // this should be wicked fast since NC_000019.10 is now cached
+        assertTrue("AGCAGCAGC".equals(gereseMgr.getRegion("NC_000019.10", Range.between(41353007, 41353015), true)));
+        logger.info("finish search for NC_000019.10:g.41353007_41353015dupAGCAGCAGC");
+    }
+
+}
---
+++ gerese4j-build-37-3/src/main/java/org/renci/gerese4j/build_37_3/GeReSe4jBuild_37_3_Manager.java
@@ -0,0 +1,122 @@
+package org.renci.gerese4j.build_37_3;
+
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.zip.GZIPInputStream;
+
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang3.Range;
+import org.renci.gerese4j.core.BuildType;
+import org.renci.gerese4j.core.GeReSe4jException;
+import org.renci.gerese4j.core.ReferenceSequence;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class GeReSe4jBuild_37_3_Manager {
+
+    private static final Logger logger = LoggerFactory.getLogger(GeReSe4jBuild_37_3_Manager.class);
+
+    private Set<String> indexSet = null;
+
+    private final Map<String, ReferenceSequence> referenceSequenceCache = new HashMap<>();
+
+    private static GeReSe4jBuild_37_3_Manager instance;
+
+    public static GeReSe4jBuild_37_3_Manager getInstance() {
+        if (instance == null) {
+            instance = new GeReSe4jBuild_37_3_Manager();
+            instance.init();
+        }
+        return instance;
+    }
+
+    private GeReSe4jBuild_37_3_Manager() {
+        super();
+    }
+
+    public BuildType getBuild() {
+        return BuildType.BUILD_37_3;
+    }
+
+    private void init() {
+        logger.debug("ENTERING init()");
+        try (InputStream is = getClass().getResourceAsStream("refseq_index.ser");
+                GZIPInputStream gzipis = new GZIPInputStream(is, Double.valueOf(Math.pow(2, 16)).intValue());
+                ObjectInputStream ois = new ObjectInputStream(gzipis)) {
+            indexSet = (Set<String>) ois.readObject();
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e);
+        }
+    }
+
+    public Set<String> getIndexSet() {
+        return indexSet;
+    }
+
+    public void setIndexSet(Set<String> indexSet) {
+        this.indexSet = indexSet;
+    }
+
+    public String getBase(String accession, int idx, boolean zeroBased) throws GeReSe4jException {
+        logger.debug("ENTERING getBase()");
+
+        if (CollectionUtils.isEmpty(indexSet)) {
+            throw new GeReSe4jException("No indices found");
+        }
+
+        if (!indexSet.contains(accession)) {
+            throw new GeReSe4jException("No accession found in indexList");
+        }
+
+        if (!this.referenceSequenceCache.containsKey(accession)) {
+            try (InputStream is = getClass().getResourceAsStream(String.format("%s.ser", accession));
+                    GZIPInputStream gzipis = new GZIPInputStream(is, Double.valueOf(Math.pow(2, 16)).intValue());
+                    ObjectInputStream ois = new ObjectInputStream(gzipis)) {
+                this.referenceSequenceCache.put(accession, (ReferenceSequence) ois.readObject());
+            } catch (Exception e) {
+                logger.error(e.getMessage(), e);
+            }
+        }
+
+        ReferenceSequence referenceSequence = this.referenceSequenceCache.get(accession);
+
+        if (zeroBased) {
+            return referenceSequence.getSequence().substring(idx - 1, idx);
+        } else {
+            return referenceSequence.getSequence().substring(idx, idx + 1);
+        }
+    }
+
+    public String getRegion(String accession, Range<Integer> range, boolean zeroBased) throws GeReSe4jException {
+        logger.debug("ENTERING getRegion()");
+
+        if (CollectionUtils.isEmpty(indexSet)) {
+            throw new GeReSe4jException("No indices found");
+        }
+
+        if (!indexSet.contains(accession)) {
+            throw new GeReSe4jException("No accession found in indexList");
+        }
+
+        if (!this.referenceSequenceCache.containsKey(accession)) {
+            try (InputStream is = getClass().getResourceAsStream(String.format("%s.ser", accession));
+                    GZIPInputStream gzipis = new GZIPInputStream(is, Double.valueOf(Math.pow(2, 16)).intValue());
+                    ObjectInputStream ois = new ObjectInputStream(gzipis)) {
+                this.referenceSequenceCache.put(accession, (ReferenceSequence) ois.readObject());
+            } catch (Exception e) {
+                logger.error(e.getMessage(), e);
+            }
+        }
+
+        ReferenceSequence referenceSequence = this.referenceSequenceCache.get(accession);
+
+        if (zeroBased) {
+            return referenceSequence.getSequence().substring(range.getMinimum() - 1, range.getMaximum());
+        } else {
+            return referenceSequence.getSequence().substring(range.getMinimum(), range.getMaximum() + 1);
+        }
+    }
+}
