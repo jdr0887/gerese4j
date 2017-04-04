5c6fadaae2315c2a7b3872d62549abb171ea1f67
Jason Reilly <jdr0887@gmail.com>
2017-04-04 08:31:32 -0400

    adding/removing builds

--- gerese4j-core/src/main/java/org/renci/gerese4j/core/BuildType.java
+++ gerese4j-core/src/main/java/org/renci/gerese4j/core/BuildType.java
@@ -4,11 +4,15 @@ public enum BuildType {
 
     BUILD_36_1("36.1"),
 
+    BUILD_36_2("36.2"),
+
+    BUILD_36_3("36.3"),
+
     BUILD_37_1("37.1"),
 
     BUILD_37_2("37.2"),
 
-    BUILD_38_2("38.2"),
+    BUILD_37_3("37.3"),
 
     BUILD_38_7("38.7");
 
