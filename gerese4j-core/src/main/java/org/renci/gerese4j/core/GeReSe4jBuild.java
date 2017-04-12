b1cb0c5afbcf11a3f99da3ce6d88614d77739d24
Jason Reilly <jdr0887@gmail.com>
2017-04-12 15:05:04 -0400

    adding accessors to cache

--- gerese4j-core/src/main/java/org/renci/gerese4j/core/GeReSe4jBuild.java
+++ gerese4j-core/src/main/java/org/renci/gerese4j/core/GeReSe4jBuild.java
@@ -9,7 +9,7 @@ public interface GeReSe4jBuild {
 
     public BuildType getBuild();
 
-    public Set<String> getIndices();
+    public Set<String> getIndices() throws GeReSe4jException;
 
     public String getHeader(String accession) throws GeReSe4jException;
 
@@ -17,8 +17,16 @@ public interface GeReSe4jBuild {
 
     public ReferenceSequence getReferenceSequence(String accession) throws GeReSe4jException;
 
+    public ReferenceSequence getReferenceSequence(String accession, boolean cache) throws GeReSe4jException;
+
     public String getBase(String accession, int idx, boolean zeroBased) throws GeReSe4jException;
 
+    public String getBase(String accession, int idx, boolean zeroBased, boolean cache) throws GeReSe4jException;
+
     public String getRegion(String accession, Range<Integer> range, boolean zeroBased) throws GeReSe4jException;
+
+    public String getRegion(String accession, Range<Integer> range, boolean zeroBased, boolean cache) throws GeReSe4jException;
+
+    public void serialize() throws GeReSe4jException;
 
 }
